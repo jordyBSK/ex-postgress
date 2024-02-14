9faa68c6ddd6c3c99f6721647a68007729e333d5
Rignchen <rinchendemort@gmail.com>
2024-02-14 15:40:03 +0100

    feat: test if user is connected

--- login/public/request.php
+++ login/public/request.php
@@ -1,3 +1,11 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Jordybsk\ExPostgress\Connection;
+
+// create connection
+$connection = new Connection();
+
+// check if user is already logged in
+if (!$connection->isConnected())
+    throw new Exception('Please log in first');
