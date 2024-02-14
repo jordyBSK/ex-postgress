7e4a0ca4b7019f27d9f31c8a90cc4c28c9db2a52
Rignchen <rinchendemort@gmail.com>
2024-02-14 13:13:06 +0100

    feat: ensure that password and username are specified

--- login/public/login.php
+++ login/public/login.php
@@ -2,6 +2,13 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Jordybsk\ExPostgress\Connection;
+
+// create a new connection
 $connection = new Connection();
+
+// check if password and username are set
+if (!isset($_POST['username'], $_POST['password'])) {
+    throw new Exception('Please enter a valid username and password');
+}
 
 $connection->connect($_POST['username'], $_POST['password']);
--- login/public/register.php
+++ login/public/register.php
@@ -2,5 +2,12 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Jordybsk\ExPostgress\Connection;
+
+// create a new connection
 $connection = new Connection();
+
+// check if username and password are set
+if (!isset($_POST['username'], $_POST['password'])) {
+    throw new Exception('Please enter a valid username and password');
+}
 
