1a743a504f4545842543580146d2ed5ffac34c77
Rignchen <rinchendemort@gmail.com>
2024-02-16 09:54:23 +0100

    feat: implement main loop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # simulate a given amount of esp32 devices that send data through a given http server at a given interval
 # syntax: python3 main.py <host> [<amount of devices>] [<interval>]
 
+from time import sleep
+from datetime import datetime
 from sys import argv
 
 # verify that the correct amount of arguments are given
@@ -12,3 +14,8 @@ if len(argv) < 2 | len(argv) > 4:
 host = argv[1]
 amount = int(argv[2]) if len(argv) > 2 else 1
 interval = int(argv[3]) if len(argv) > 3 else 60
+
+# simulate the devices
+while True:
+	start = datetime.now()
+	sleep(interval - (datetime.now() - start).total_seconds())
