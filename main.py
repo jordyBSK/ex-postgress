0eeedb9f0023ce4dae2d040079c60d84b386ba71
Rignchen <rinchendemort@gmail.com>
2024-02-16 09:52:44 +0100

    feat: store the arguments

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ from sys import argv
 if len(argv) < 2 | len(argv) > 4:
 	print("Syntax: python3 main.py <host> [<amount of devices>] [<interval>]")
 	exit(1)
+
+# store the given arguments
+host = argv[1]
+amount = int(argv[2]) if len(argv) > 2 else 1
+interval = int(argv[3]) if len(argv) > 3 else 60
