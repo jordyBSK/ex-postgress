369b2ff9d5a0f2a2fa8e5f486cdde62da1937b90
Rignchen <119439839+Rignchen@users.noreply.github.com>
2024-02-19 10:36:59 +0100

    Esp32 (#6)
    * feat: change python script to permanently generate data for a random device
    
    * feat: describe script in readme
    
    * feat: rename main.py for a more explicit name
    
    * feat: clear shell
    
    * feat: make printed data smaller and more readable
    
    * feat: display request number
    
    * feat: send first request
    
    * feat: try to send json encoded values dictionnary (not workng)
    
    * refactor: remove unused import
    
    * feat: display average request duration
    
    * fix: send values 1 at the time
    
    * feat. update readme
    
    ---------
    
    Co-authored-by: jordyBSK <125542402+jordyBSK@users.noreply.github.com>

--- esp32.py
+++ esp32.py
@@ -1,40 +1,58 @@
 # simulate a given amount of esp32 devices that send data through a given http server at a given interval
-# syntax: python3 main.py <host> [<amount of devices>] [<interval>]
+# syntax: python3 main.py <host> [<amount of devices>]
 
 import requests
-from time import sleep
+
 from random import randint
 from json import dumps
 from datetime import datetime
 from sys import argv
+from os import system, name
+
+if name == "nt":
+	def cls(): system("cls")
+else: 
+	def cls(): system("clear")
+def average(data: list): return sum(data) / len(data)
 
 # verify that the correct amount of arguments are given
-if len(argv) < 2 | len(argv) > 4:
-	print("Syntax: python3 main.py <host> [<amount of devices>] [<interval>]")
+if len(argv) < 2 or len(argv) > 3:
+	print("Syntax: python3 main.py <host> [<amount of devices>]")
 	exit(1)
 
 # store the given arguments
 host = argv[1]
 amount = int(argv[2]) if len(argv) > 2 else 1
-interval = int(argv[3]) if len(argv) > 3 else 60
 
-input(f"Simulating {amount} devices sending data to {host} every {interval} seconds\nPress enter to start...")
+# create display variables
+i = 0
+times = []
+
+cls()
+input(f"Simulating {amount} devices sending data to {host}\nPress enter to start...")
+cls()
 
 # simulate the devices
 try:
 	while True:
 		start = datetime.now()
-		values = []
-		while (datetime.now() - start).total_seconds() < interval:
-			values.append({
-				"device_id": randint(0, amount - 1),
-				"timestamp": datetime.now().isoformat(),
-				"temperature": randint(0, 40),
-				"humidity": randint(0, 100),
-				"light": randint(0, 1000)
-			})
-		requests.post(f"{host}", data=dumps(values))
-		print(f"{datetime.now().isoformat()} {dumps(values, indent=4)}")
+		# get random values
+		values = {
+			"device_id": randint(0, amount - 1),
+			"timestamp": str(datetime.now()),
+			"temperature": randint(0, 40),
+			"humidity": randint(0, 100),
+			"light": randint(0, 1000)
+		}
+
+		# send the data to the server
+		requests.post(host, data=values)
+
+		# display the data
+		print(i := i + 1)
+		print(f"{str(datetime.now())[:-5]}: data sent: {dumps(values)}")
+		times.append((datetime.now() - start).total_seconds())
+		print(f"Average request time: {average(times)} seconds\n")
 except KeyboardInterrupt:
 	print("Exiting...")
 	exit(0)
