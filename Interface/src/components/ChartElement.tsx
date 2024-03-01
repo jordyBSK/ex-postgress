94a23ced15e99a80e01c755b2073e4edef16366b
jordybsk <dylan.bossoku@jobtrek.ch>
2024-03-01 09:22:35 +0100

    feat: display the correct number of days on the graph for the month selected

--- Interface/src/components/ChartElement.tsx
+++ Interface/src/components/ChartElement.tsx
@@ -1,11 +1,12 @@
-import {useEffect, useRef} from 'react';
+import {useEffect, useRef, useState} from "react";
 import Chart from "chart.js/auto";
 import zoomPlugin from 'chartjs-plugin-zoom';
 Chart.register(zoomPlugin);
-export function ChartElement({ monthNames, temperatureAverages, humidityAverages, monthSelect }: { monthNames: string | string[] , temperatureAverages: number[]|number, humidityAverages: number[] |number, monthSelect: (month: string) => void}) {
+
+export function ChartElement({ monthNames, temperatureAverages, humidityAverages, monthSelect }: { monthNames: string[] | number[], temperatureAverages: number[] | number, humidityAverages: number[] | number, monthSelect: (month: string) => void }) {
     const chartRef = useRef<HTMLCanvasElement>(null);
     const chartInstance = useRef<Chart>();
-    const [selectedMonth, setSelectedMonth] = useState<number[]>();
+    const [selectedMonth, setSelectedMonth] = useState<number[]>([]);
 
     useEffect(() => {
         if (chartRef && chartRef.current) {
@@ -15,26 +16,24 @@ export function ChartElement({ monthNames, temperatureAverages, humidityAverages
                     chartInstance.current.destroy();
                 }
 
+                const labels = Array.isArray(monthNames) ? monthNames : [monthNames];
                 chartInstance.current = new Chart(ctx, {
                     type: 'line',
                     data: {
-                        labels: monthNames,
-                        datasets: [
-                            {
-                                label: 'Temperature',
-                                data: temperatureAverages,
-                                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                                borderColor: 'rgba(255, 99, 132, 1)',
-                                borderWidth: 1
-                            },
-                            {
-                                label: 'Humidity',
-                                data: humidityAverages,
-                                backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                                borderColor: 'rgba(54, 162, 235, 1)',
-                                borderWidth: 1
-                            }
-                        ]
+                        labels: labels,
+                        datasets: [{
+                            label: 'Temperature Averages',
+                            data: temperatureAverages,
+                            borderColor: 'rgb(255, 99, 132)',
+                            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                            tension: 0.1
+                        }, {
+                            label: 'Humidity Averages',
+                            data: humidityAverages,
+                            borderColor: 'rgb(54, 162, 235)',
+                            backgroundColor: 'rgba(54, 162, 235, 0.5)',
+                            tension: 0.1
+                        }]
                     },
                     options: {
                         scales: {
@@ -46,32 +45,33 @@ export function ChartElement({ monthNames, temperatureAverages, humidityAverages
                             zoom: {
                                 zoom: {
                                     wheel: {
-                                        enabled: true
+                                        enabled: true,
                                     },
                                     pinch: {
                                         enabled: true
                                     },
-                                    mode: 'y',
+                                    mode: 'xy',
                                 }
                             }
-                        },   onClick: function (_event, elements) {
+                        }, onClick: function (event, elements) {
                             if (elements.length > 0) {
                                 const clickedElement = elements[0];
                                 const index = clickedElement.index;
-                                const monthClicked : string | number = monthNames[index];
+                                const monthClicked: string | number = labels[index];
                                 setSelectedMonth(monthClicked);
-                                monthSelect(monthClicked)
+                                monthSelect(monthClicked.toString());
                             }
                         }
                     }
                 });
             }
         }
-    }, [chartRef, monthNames, temperatureAverages, humidityAverages, monthSelect]);
+
+    }, [monthNames, temperatureAverages, humidityAverages]);
 
     return (
         <div>
-            <canvas ref={chartRef} width="800" height="400"></canvas>
+            <canvas ref={chartRef} width="600" height="400"></canvas>
         </div>
     );
 }
--- Interface/src/components/MonthAverageStore.tsx
+++ Interface/src/components/MonthAverageStore.tsx
@@ -1,4 +1,5 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
+import { ChartElement } from "./ChartElement.tsx";
 
 export function MonthAverageStore() {
     interface Data {
@@ -8,52 +9,49 @@ export function MonthAverageStore() {
     }
 
     const [data, setData] = useState<Data[]>([]);
-    const [monthSelected, setMonthSelected] = useState<string>('March');
-    let temperatureAverage = 0;
-    let humidityAverage = 0
+    const [monthSelected, setMonthSelected] = useState<string>('January');
+    let tempInMonth = [2, 3, 2, 5, 2, 4, 2, 2, 6, 7, 9];
+    let humidInMonth = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
 
-        useEffect(() => {
-            fetch('http://192.168.1.66:3000/data')
-                .then(response => response.json())
-                .then((apiData: Data[]) => {
-                    setData(apiData);
-                })
-                .catch(error => {
-                    console.error('Une erreur s\'est produite:', error);
-                });
-        }, []);
-
-    function getAverageOfMonthSelected() {
-        if (!monthSelected) {
-            console.error("Month not selected.");
-            return;
-        }
-
-        const selectedMonthData = data.filter(entry => {
-            const numberMonth = new Date(entry.timestamp).toLocaleString('en-US', {month: 'long'});
-            return numberMonth === monthSelected;
-        });
-
-        const temperatureSum = selectedMonthData.reduce((acc, entry) => acc + entry.temperature, 0);
-        const humiditySum = selectedMonthData.reduce((acc, entry) => acc + entry.humidity, 0);
-        temperatureAverage = temperatureSum / selectedMonthData.length;
-        humidityAverage = humiditySum / selectedMonthData.length;
-
-        console.log(`Average Temperature for ${monthSelected}: ${temperatureAverage}`);
-        console.log(`Average Humidity for ${monthSelected}: ${humidityAverage}`);
-    }
+    const [dayInMonth, setDayInMonth] = useState<number[]>([]);
 
     useEffect(() => {
-        getAverageOfMonthSelected();
-    }, [data, monthSelected]);
+        fetch('http://192.168.1.66:3000/data')
+            .then(response => response.json())
+            .then((apiData: Data[]) => {
+                setData(apiData);
+            })
+            .catch(error => {
+                console.error('Une erreur s\'est produite:', error);
+            });
+    }, []);
+
+    useEffect(() => {
+        const date = new Date();
+        const year = date.getFullYear();
+        const monthIndex = getMonthIndex(monthSelected);
+        const days = daysInMonth(monthIndex, year);
+        const daysArray = Array.from({ length: days }, (_, i) => i + 1);
+        setDayInMonth(daysArray);
+    }, [monthSelected]);
+
+    function getMonthIndex(month: string): number {
+        const months: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        return months.indexOf(month);
+    }
+
+    function daysInMonth(month: number, year: number): number {
+        return new Date(year, month + 1, 0).getDate();
+    }
 
     const monthSelect = (month: string) => {
         setMonthSelected(month);
-        console.log(monthSelected);
     };
 
     return (
         <>
+            <ChartElement monthNames={dayInMonth} temperatureAverages={tempInMonth}
+                          humidityAverages={tempInMonth} monthSelect={monthSelect} />
         </>
     );
 }
