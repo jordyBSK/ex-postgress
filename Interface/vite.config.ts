2247084270354b6de591d462bcdb8ba2ffb16064
jordybsk <dylan.bossoku@jobtrek.ch>
2024-03-05 16:11:44 +0100

    feat: add button from shadcn library

--- Interface/vite.config.ts
+++ Interface/vite.config.ts
@@ -4,4 +4,9 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
