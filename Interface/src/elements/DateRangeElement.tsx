3926701731268b53d0a6d1f63f763ac0d0af2a1c
jordybsk <dylan.bossoku@jobtrek.ch>
2024-03-08 11:57:51 +0100

    feat: get all days in date range

--- Interface/src/elements/DateRangeElement.tsx
+++ Interface/src/elements/DateRangeElement.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import { ChartElement } from "./ChartElement.tsx";
+import CardElement from "./CardElement.tsx";
 
 interface Data {
     temperature: number;
@@ -7,26 +9,41 @@ interface Data {
 }
 
 export function DateRangeElement({ data }: { data: Data[] }) {
-    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     const [startDate, setStartDate] = useState<Date | null>(null);
     const [endDate, setEndDate] = useState<Date | null>(null);
-
+    const [dateRange, setDateRange] = useState<string[]>([]);
 
     useEffect(() => {
-
+        if (startDate && endDate) {
+            generateDateRange();
+        }
     }, [startDate, endDate, data]);
 
+    function generateDateRange() {
+        if (startDate && endDate) {
+            const dates: string[] = [];
+            const start = new Date(startDate);
+
+            while (start <= endDate) {
+                dates.push(start.toISOString().split('T')[0]);
+                start.setDate(start.getDate() + 1);
+            }
+            setDateRange(dates);
+        }
+    }
 
     return (
         <>
             <div>
-                <label>Start Date:</label>
+                <label>Date de début :</label>
                 <input type="date" value={startDate ? startDate.toISOString().split('T')[0] : ''} onChange={e => setStartDate(new Date(e.target.value))} />
             </div>
             <div>
-                <label>End Date:</label>
+                <label>Date de fin :</label>
                 <input type="date" value={endDate ? endDate.toISOString().split('T')[0] : ''} onChange={e => setEndDate(new Date(e.target.value))} />
             </div>
+            {dateRange}
+            <CardElement description="Moyenne mensuelle" theme="Chart" element={<ChartElement monthNames={dateRange} />} />
         </>
     );
 }
