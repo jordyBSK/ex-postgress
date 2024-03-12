918b62f9b9159ccb6a4bb2e0e458627a81bb3880
Rignchen <rinchendemort@gmail.com>
2024-03-12 16:47:57 +0100

    feat: add authentification to esp32

--- backend/init.sql
+++ backend/init.sql
@@ -7,13 +7,13 @@ create table api.data (
   id character varying(14) NOT NULL
 );
 
--- create role
-create role web_anon nologin;
-grant usage on schema api to web_anon;
-grant select, insert on api.data to web_anon;
-
 -- create autenticator
 create role authenticator noinherit login password 'mysecretpassword';
+
+-- create anonymous role
+create role web_anon nologin;
+grant usage on schema api to web_anon;
+grant select on api.data to web_anon;
 grant web_anon to authenticator;
 
 -- convert unix timestamp to timestamp and insert it
@@ -26,3 +26,9 @@ create or replace function api.insert_data(
 returns void as $$
 	insert into api.data("temperature", "humidity", "id", "timestamp") values (temperature, humidity, id, to_timestamp(unix_timestamp));
 $$ language sql;
+
+-- create role for the esp 32
+create role esp32 nologin;
+grant usage on schema api to esp32;
+grant insert on api.data to esp32;
+grant esp32 to authenticator;
