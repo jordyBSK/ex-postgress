2c1364f5e0f42649f10b28495bb7e8bbe5f5e47b
Rignchen <rinchendemort@gmail.com>
2024-03-12 11:15:35 +0100

    feat: esp32 now send timestamp

--- backend/init.sql
+++ backend/init.sql
@@ -3,7 +3,7 @@ create schema if not exists api;
 create table api.data (
   temperature real,
   humidity real,
-  timestamp timestamp NOT NULL DEFAULT now(),
+  timestamp timestamp NOT NULL,
   id character varying(14) NOT NULL
 );
 
@@ -16,3 +16,13 @@ grant select, insert on api.data to web_anon;
 create role authenticator noinherit login password 'mysecretpassword';
 grant web_anon to authenticator;
 
+-- convert unix timestamp to timestamp and insert it
+create or replace function api.insert_data(
+	temperature real,
+	humidity real,
+	id varchar(15),
+	unix_timestamp bigint
+) 
+returns void as $$
+	insert into api.data("temperature", "humidity", "id", "timestamp") values (temperature, humidity, id, to_timestamp(unix_timestamp));
+$$ language sql;
