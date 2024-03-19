1c7cbdb95c1f72d7d7e881262d8c34e61fc15aed
Rignchen <rinchendemort@gmail.com>
2024-03-19 09:46:28 +0100

    feat: ensure that the user gave password and username

--- backend/login/public/login.php
+++ backend/login/public/login.php
@@ -1,2 +1,11 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+
+$data = match ($_SERVER['REQUEST_METHOD']) {
+	'GET' => $_GET,
+	'POST' => $_POST,
+	default => output(['error' => 'Unsupported method'], 405),
+};
+
+if (!isset($data['username']) || !isset($data['password']))
+    output(['error' => 'Username and password are required'], 400);
