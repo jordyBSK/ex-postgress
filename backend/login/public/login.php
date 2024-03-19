bb97f468bf5bcf9f1e2b636bbc9b5744dbf7549e
Rignchen <rinchendemort@gmail.com>
2024-03-19 10:39:25 +0100

    feat: load environment variables

--- backend/login/public/login.php
+++ backend/login/public/login.php
@@ -1,6 +1,8 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Symfony\Component\Dotenv\Dotenv;
+
 $data = match ($_SERVER['REQUEST_METHOD']) {
 	'GET' => $_GET,
 	'POST' => $_POST,
@@ -9,3 +11,7 @@ $data = match ($_SERVER['REQUEST_METHOD']) {
 
 if (!isset($data['username']) || !isset($data['password']))
     output(['error' => 'Username and password are required'], 400);
+
+// Load environment variables
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__ . '/../.env');
